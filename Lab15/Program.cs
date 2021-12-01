5b35aab40e4dc5e106990d646e97dded6df77412
Kseniia <balerinagizelle@mail.ru>
2021-12-02 00:11:27 +0300

    Add project files.

---
+++ Lab15/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArithProgression arith1 = new ArithProgression(1, 5);
+            arith1.setStart(5);
+            Console.WriteLine(arith1.getNext()); 
+            arith1.reset();
+            GeomProgression geom1 = new GeomProgression(2,6);
+            geom1.setStart(6);
+            geom1.getNext();
+            geom1.reset();
+            Console.ReadKey();
+
+
+
+        }
+
+
+    }
+    interface ISeries
+    {
+
+
+        void setStart(int x);
+        int getNext();
+        void reset();
+    }
+    class ArithProgression : ISeries
+    {
+
+
+        public int a1;
+
+        public int d;
+        public ArithProgression(int D, int A1)
+        {
+            d = D;
+            a1 = A1;
+        }
+
+        public void setStart(int a1)
+        {
+
+            Console.WriteLine("Первый член арифметической прогрессии: {0}", a1);
+
+        }
+
+        public int getNext()
+        {
+            int result = a1 + d;
+            return result;
+        }
+
+        public void reset()
+        {
+            Console.WriteLine("Первый член арифметической прогрессии: {0}", a1);
+        }
+
+
+    }
+    class GeomProgression : ISeries
+    {
+
+
+        public int b1;
+        public int q;
+        public GeomProgression(int Q, int B1)
+        {
+            q = Q;
+            b1 = B1;
+        }
+
+        public void setStart(int b1)
+        {
+
+            Console.WriteLine("Первый член арифметической прогрессии: {0}", b1);
+
+        }
+
+        public int getNext()
+        {
+            return b1 * q;
+        }
+
+        public void reset()
+        {
+            Console.WriteLine("Первый член арифметической прогрессии: {0}", b1);
+        }
+
+
+    }
+
+}
